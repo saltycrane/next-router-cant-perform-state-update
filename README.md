a8588e607b9011a554ffbf4ca4a4b26e863ccf5f
Eliot <saltycrane@gmail.com>
2022-03-15 10:10:06 -0700

    add example that causes warning

--- README.md
+++ README.md
@@ -7,4 +7,14 @@ $ npm install
 $ npm run dev
 ```
 
-Go to http://localhost:3000 in the browser
+## To see the warning
+
+- open the browser devtools console
+- go to http://localhost:3000/?tab=tab1
+- reload the page
+- click the "Tab 2" button
+- see the following warning
+
+``` 
+Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
+```
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,62 @@
-export default function Index() {
-  return <div>Index</div>;
-}
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+/**
+ * Index
+ */
+const Index = () => {
+  const router = useRouter();
+
+  return (
+    <div>
+      <button onClick={() => router.push({ query: { tab: "tab1" } })}>
+        Tab 1
+      </button>
+      <button onClick={() => router.push({ query: { tab: "tab2" } })}>
+        Tab 2
+      </button>
+      {router.query.tab !== "tab2" && <MyTab1Content />}
+    </div>
+  );
+};
+
+export default Index;
+
+/**
+ * MyTab1Content
+ */
+const MyTab1Content = () => {
+  const [, setCount] = useState(0);
+  const increment = useCallback(() => setCount((count) => count + 1), []);
+
+  return <MyWidget increment={increment} />;
+};
+
+/**
+ * MyWidget
+ */
+type TProps = {
+  increment: () => void;
+};
+
+const MyWidget = ({ increment }: TProps) => {
+  const isMountedRef = useRef(false);
+
+  useEffect(() => {
+    isMountedRef.current = true;
+
+    const handleClick = () => {
+      if (!isMountedRef.current) {
+        return;
+      }
+      increment();
+    };
+    document.addEventListener("click", handleClick);
+    return () => {
+      isMountedRef.current = false;
+      document.removeEventListener("click", handleClick);
+    };
+  }, [increment]);
+
+  return <div>My widget</div>;
+};
